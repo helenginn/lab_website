00b101f33e26689347f29f6070d3641aaa8828d5
Helen Ginn <github@hginn.co.uk>
2023-06-20 11:39:47 +0200

    Change Helen's employer

--- faces/helen.php
+++ faces/helen.php
@@ -5,7 +5,7 @@
 <div class="persondesc">
 <div class="name">Helen</div>
 <div class="blurb">
-The Protein Machinists lab is led by Dr. Helen Ginn. She is a senior research scientist at Diamond Light Source in the UK. She has delivered the software suite Vagabond for refining protein structures in torsion angle space and cluster4x for comparing related crystal structures. She also has a scientific background in virology, working on the immune escape of SARS-CoV-2 variants and structural biology efforts against foot-and-mouth disease virus. Her doctorate was spent in reciprocal space at the University of Oxford working on improved algorithms for data reduction for serial crystallography. Her hobbies include dress-making, leathercraft and blacksmithing.
+The Protein Machinists lab is led by Dr. Helen Ginn. She is a Helmholtz Young Investigator at Deutsches Elektronen-Synchrotron, DESY. She has delivered the software suite Vagabond for refining protein structures in torsion angle space and cluster4x for comparing related crystal structures. She also has a scientific background in virology, working on the immune escape of SARS-CoV-2 variants and structural biology efforts against foot-and-mouth disease virus. Her doctorate was spent in reciprocal space at the University of Oxford working on improved algorithms for data reduction for serial crystallography. Her hobbies include dress-making, leathercraft and blacksmithing.
 </div>
 </div>
 </div>
